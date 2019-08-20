7c4b366c46780d40b1f86b895e5136ffc8a56e8d
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2019-08-20 17:12:14 +0530

    setup: herekou in progress, derp

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
   dbName: 'AttendancePortal',
   database: {
-    url: `mongodb://localhost:27017/muj/${this.dbName}`,
+    // url: `mongodb://localhost:27017/muj/${this.dbName}`,
+    url: 'mongodb://svr8:mujdb19@ds155396.mlab.com:55396/muj',
     mongoOptions: { useNewUrlParser: true },
   },
 }
